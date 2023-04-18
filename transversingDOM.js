7197e22a38f76cd408767c6e70ed5af20446eb49
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-17 22:27:48 -0400

    1

---
+++ transversingDOM.js
@@ -0,0 +1,15 @@
+//Get the header element
+const el = document.getElementsByTagName("header");
+console.log(el);
+//Get all the section elements
+const sections = document.getElementsByTagName("section");
+console.log(sections);
+//Get the section element with class="current"
+
+//Get the section that comes after the current section
+
+//Get the h2 node from the section before the 'current' section
+
+//Get the div that contains the section that has an h2 with a class of 'highlight'
+
+//Get all the sections that contain an H2 (using a single statement);
