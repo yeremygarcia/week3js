7e77f8d1bb954496a163144d519084998b4b8ba9
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-20 22:50:30 -0400

    1

---
+++ Mock/mock.js
@@ -0,0 +1,38 @@
+ // Create an array of user objects called users. Each user object should have an id, name, and email property.
+const users = [
+    { id: 1, name: "Jim", email: "jim@gmail.com" },
+    { id: 2, name: "Bob2", email: "bob@gmail.com" },
+    { id: 3, name: "Joe", email: "joe@gmail.com" }
+  ];
+  
+// Implement a function called fetchUserById that accepts a user ID as an argument and returns a promise.
+  function fetchUserById(id) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const user = users.find(user => user.id === id);
+        if (user) {
+          resolve(user);
+        } else {
+          reject(new Error ("User not found"));
+        }
+      }, 1000);
+    });
+  }
+  
+  // Use fetchUserById to fetch the user with ID 1
+  fetchUserById(1)
+    .then((user) => {
+        console.log("Retrieved user: ", user);
+    })
+    .catch((error) => {
+        console.log("Error retrieving user: ", error);
+    });
+  
+  // Use fetchUserById to fetch the user with ID 2
+  fetchUserById(2)
+    .then((user) => {
+        console.log("Retrieved user: ", user);
+    })
+    .catch((error) => {
+        console.log("Error retrieving user: ", error);
+    });
